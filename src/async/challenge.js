2ff9eb5fb103a931ba3807f4059399a7d1d235ca
Antonio Berardi <antoberar@gmail.com>
2021-07-10 19:26:29 -0400

    Async:Challenge

---
+++ src/async/challenge.js
@@ -0,0 +1,23 @@
+// import { fecthData } from "";
+const fecthData = require("../utils/fetchData");
+
+const API = "https://rickandmortyapi.com/api/character/";
+
+const anotherFunction = async (url_api) => {
+    try {
+        const data = await fecthData(url_api);
+        const character = await fecthData(`${API}${data.results[0].id}`);
+        const origin = await fecthData(character.origin.url);
+        console.log(data.info.count);
+        console.log(character.name);
+        console.log(origin.dimension);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+
+console.log('Before');
+anotherFunction(API);
+console.log('After');
---
+++ src/async/index.js
@@ -0,0 +1,37 @@
+const doSomethingAsync = () => {
+  return new Promise((resolve, reject) => {
+    (true)
+      ? setTimeout(() => {
+          resolve("Do something Async");
+        }, 3000)
+      : reject(new Error("Test error"));
+  });
+};
+
+
+const doSomething = async () => {
+    const something = await doSomethingAsync();
+    console.log(something);
+};
+
+
+console.log('Before 1');
+doSomething();
+console.log('After 1');
+
+
+const anotherFunction = async () => {
+    try {
+        const something = await doSomethingAsync();
+        console.log(something);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+
+
+console.log('Before 2');
+anotherFunction();
+console.log('After 2');
